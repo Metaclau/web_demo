f5cf09bd46ca46767b3bf419cb28695678644b54
claudio.metalia <tg-metalia@outlook.de>
2024-07-13 14:59:58 +0200

    test Button

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,6 +10,14 @@ public class IndexController {
     
     @GetMapping(value = {"/", "index.html"})
     public String index(ModelAndView model){
+        return "views/index";
+    }
+
+    @GetMapping("/your-endpoint")
+    public String handleRequest(@RequestParam(name = "map") String mapParam) {
+        //BackEnd:
+        System.out.println(mapParam);
+        
         return "views/index";
     }
 }
